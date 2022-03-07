216e353ce2f47e2dadb60905bbbcbd3533b3a8ad
DESKTOP-STON0IN\Navid Bakhtiary <navidbakhtiary@yahoo.com>
2022-03-07 11:03:28 +0330

    fix: change plugin url

--- Page-Words-Count.php
+++ Page-Words-Count.php
@@ -1,12 +1,13 @@
 <?php
 /*
 Plugin Name:  Page Words Count
-Plugin URI:   https://www.wordpress.org 
+Plugin URI:   https://github.com/navidbakhtiary/WordPress-PWC-Plugin 
 Description:  a light plugin for add page words count column to list of pages
 Version:      1.0
 Author:       Navid Bakhtiary 
 Author URI:   https://www.linkedin.com/in/navid-bakhtiary/
 */
+
 add_filter('manage_pages_columns', 'pwc_plugin_add_words_count_column');
 add_action('manage_pages_custom_column', 'pwc_plugin_fill_words_count_column', 5, 2);
 
